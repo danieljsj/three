57efd68cd80826cad9ec07b28329b23bc5424157
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2018-07-17 16:46:35 -0500

    added reminder message (to use trailing slashes)

--- steal.bash
+++ steal.bash
@@ -1,14 +1,24 @@
 #!/bin/bash
 echo
-echo "The way to use this script is to specify a file or directory within 'theirs' that you want to pull into 'ours'."
-echo "... for example:"
-echo " steal.bash theirs/examples/webgl_loader_collada.html"
-echo "the script will use 'makedir -p' to make sure our corresponding parent directory is there, and rsync to sync the item into that corresponding path."
-echo "press ctrl+c to cancel if you're concerned."
+# echo "The way to use this script is to specify a file or directory within 'theirs' that you want to pull into 'ours'."
+# echo "... for example:"
+# echo " steal.bash theirs/examples/webgl_loader_collada.html"
+# echo "the script will use 'makedir -p' to make sure our corresponding parent directory is there, and rsync to sync the item into that corresponding path."
+# echo "press ctrl+c to cancel if you're concerned."
+# echo
+
+# read
+
+echo
+echo
+echo "REMEMBER TO:"
+echo "- Use trailing slashes if copying just a dir. (just like you need to when using rsync)"
+echo
 echo
 
-read
-
+echo
+echo "PROCESSING:"
+echo
 their_path="$1"
 
 echo their path is:.. "$their_path"
@@ -20,10 +30,19 @@ our_dir=$(dirname "$our_path")
 echo our dir is:..... "$our_dir"
 
 
-mkdir -p "$our_dir"
+mkdir -pv "$our_dir"
 echo
-echo "assured our corresponding parent folder is there"
+echo "assured our corresponding parent folder '$our_dir' is there"
+echo
 
-rsync -r "$their_path" "$our_path"
+echo
+echo rsync -rv "$their_path" "$our_path"
+echo rsync -rv "$their_path" "$our_path"
+echo rsync -rv "$their_path" "$our_path"
+echo rsync -rv "$their_path" "$our_path"
+echo
+
+     rsync -rv "$their_path" "$our_path"
 echo
 echo "rsync'd their thing into our corresponding parent folder"
+echo
