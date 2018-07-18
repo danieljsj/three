b5da68b42cde2dbcd30373069a72bc061220395a
Daniel Schulz-Jackson <danielschulzjackson@gmail.com>
2018-07-17 19:22:59 -0500

    added clone.bash to get the examples from the repo

---
+++ clone.bash
@@ -0,0 +1,9 @@
+echo "you are about to do this:"
+echo rm -rfv theirs
+echo git clone https://github.com/mrdoob/three.js.git theirs
+read
+
+
+
+	 rm -rfv theirs
+	 git clone https://github.com/mrdoob/three.js.git theirs
